841a1b354d663c5aa63fd74f12b7204a4e0c553b
Thanapatsir@gmail.com <Thanapat@Thanapats-MacBook-Pro.local>
2016-10-11 04:41:21 +0700

    successfully download file but still slow, will figure out tmr

--- p1http.py
+++ p1http.py
@@ -1,32 +1,53 @@
 import socket
 import os
 import sys
-
-def make_http_req(host):
-	return ("GET / HTTP/1.1\r\nHost: {h}\r\n\r\n"). format(h=host)
+import urlparse
+import time
+def make_http_req(host,obj):
+	return ("GET {o} HTTP/1.1\r\nHost: {h}\r\n\r\n"). format(o=obj,h=host)
 
 if len(sys.argv)==4:
+	
 	file_name = str(sys.argv[2])
-	host_name = sys.argv[3]
+	url_object = urlparse.urlparse(sys.argv[3])
 
-	rq = make_http_req(host_name)
+
+	url = sys.argv[3]
+	url2 = url[7:]
+	if url_object.port==None:
+		url3 = url2.find("/")
+
+	else:
+		url3 = url2.find(":")
+	host_name = url2[:url3]
+
+
+	path = url_object.path
+		
+
+	rq = make_http_req(host_name,path)
+
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.connect((host_name, 80))
+
+	if url_object.port==None:
+		s.connect((host_name, 80))
+	else:
+		s.connect((host_name, url_object.port))
+	start = time.time()	
 	s.send(rq)
+
 	result = s.recv(8192)
 
-	# L_index = result.find('Content-Length: ')
-	# R_index = result.find('\r\n\r\n')
-	# file_size = result[L_index+16:R_index]
-
+	index = result.find('\r\n\r\n')
+	result_wo_header = result[index+4:]
 	file2write = open(file_name, 'w')
+	file2write.write(result_wo_header)
 
 	while (len(result) > 0):
-	    print(result)
-	    file2write.write(result)
 	    result = s.recv(8192)
-	    
-
+	    file2write.write(result)
+	end = time.time()
+	print"time complex: ",(end - start)
 	print "dont recieveing"
 elif len(sys.argv)==5:
 	print "not support yet, coming soon"
