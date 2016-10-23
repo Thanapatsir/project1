aaddbc4a200f7e8118b6f4cfe1e5bebb65c62d5a
Thanapatsir@gmail.com <thanapatsir>
2016-10-24 05:30:35 +0700

    done

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 # project1
 # The filename 'srget' is the most up-to-date version
 
-Basically the script take 3 inputs from  the command line, namely "-o" "outputfile" and "url"<br />
-The user must input the 3 inputs in order for the script to work<br />
-Belows are the example output (also a test cases used)<br />
-The script handle most cases such as "connection problem" or "keyboard interrupt"<br />
-If download is interrupted, user must re send the same command to resume the download<br />
 
-for testing<br />
-
- > /srget -o dsf.jpg http://plusquotes.com/images/quotes-img/cool-pictures-24.jpg <br />
- > something happen like keyboard interrupt <br />
- > /srget -o dsf.jpg http://plusquotes.com/images/quotes-img/cool-pictures-24.jpg <br />
+This is the final version of the srget:<br />
+Remark:<br />
+1. If not connection given, the program will assume 1 connection as the default setting<br />
+Features:<br />
+1. command-line HTTP downloader
+2. Concurrent downloader<br />
+3. Concurrent resume<br />
+4. Concurrent handle KeyboardInterrupt,ConnectionError (if occurred, user must re-initiate to resume)<br />
--- srget.py
+++ srget.py
@@ -1,295 +1,315 @@
+#!/usr/bin/env python
 import socket
 import os
 import sys
 import urlparse
-import os.path
+import threading
+import time
 
-def make_http_req(host,obj):
-	return ("GET {o} HTTP/1.1\r\nHost: {h}\r\nConnection: close\r\n\r\n"). format(o=obj,h=host)
-def make_resume_req(host,obj,_byte):
-    return ("GET {o} HTTP/1.1\r\n"+ "Host: {h}\r\n"+"Connection: close\r\n"+"Range: {b}\r\n\r\n"). format(o=obj,h=host,b=_byte)
 
-def index_finder(str_,word):
-	return str_.find(word)
-def clean_up(file_name):
-	dirr = os.getcwd()
-	return os.remove(dirr+"/"+file_name)
+def make_resume_req(host, obj, _byte):
+    return ("GET {o} HTTP/1.1\r\n" + "Host: {h}\r\n" + "Connection: close\r\n" + "Range: {b}\r\n\r\n"). format(o=obj, h=host, b=_byte)
+def make_http_req(host, obj):
+	return ("GET {o} HTTP/1.1\r\nHost: {h}\r\nConnection: close\r\n\r\n"). format(o=obj, h=host)
 
-def check_input():
-	if len(sys.argv)==4:
-		return True
-	else:
-		print "invalid input"
-		return False
+def normal_download(file_name, url):
+    """ DECLARE VARIABLE """
+    num_bytes_recv=0
+    file_name = str(file_name)
+    url_object = urlparse.urlparse(url)
+    path = url_object.path
+    url = url
+    url2 = url[7:]
+    if url_object.port==None:
+    	url3 = url2.find("/")
+    else:
+    	url3 = url2.find(":")
+    host_name = url2[:url3]
+
+    """ MAKE CONNECTION """
+    rq = make_http_req(host_name,path)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    if url_object.port==None:
+    	s.connect((host_name, 80))
+    else:
+    	s.connect((host_name, url_object.port))
+    s.send(rq)
+
+    """ RECV FIRST DATA WITHOUT HEADER """
+    data = ""
+    header_check = ""
+    while "\r\n\r\n" not in header_check:
+    	result = s.recv(1)
+    	header_check += result
+
+
+    """ RECV THE REST OF CONTENT """
+
+    with open(file_name, 'wb') as f:
+    	while True:#(len(result) > 0):
+    		print "number of byte recieve : ",num_bytes_recv
+    		result = s.recv(8192)
+    		if not result:
+    			break
+    		num_bytes_recv+=len(result)
+    		data+=result
+    	f.write(data)
+    print "download complete"
+
+    s.close()
+
+def make_http_req(host, obj):
+    return ("GET {o} HTTP/1.1\r\nHost: {h}\r\nConnection: close\r\n\r\n"). format(o=obj, h=host)
+
+
 def check_file_exist(file_name):
-	dirr = os.getcwd()
-	if os.path.isfile(dirr+"/"+file_name)==True: #and os.path.isfile(dirr+"/"+file_name+"_tail.txt")==True:
-		return True
-	else:
-		return False
-
-
-
-
-
-def get_resume_info(): #return true if it can be resume
-	result_list = ""
-	with open(sys.argv[2]+"_tail.txt", 'r') as f:
-		for line in f:
-			result_list += line
-	return result_list #0=content_length 1=accept-range 2=identifier 3=content wrote
-
-def resume_download(part_file_content): #resume_download(content_length,accept_range,identifier,content_wrote)
-
-
-	file_name = str(sys.argv[2])
-	url_object = urlparse.urlparse(sys.argv[3])
-	path = url_object.path
-	url = sys.argv[3]
-	url2 = url[7:]
-	url3 = url2.find("/")
-	host_name = url2[:url3]
-
-	""" EXTRACT INPUT VARIABLE """
-	L_slice = ""
-	R_slice = ""
-	tail = ""
-
-
-	L_slice = part_file_content[index_finder(part_file_content,'Content-Length:'):]
-	R_slice = L_slice.find('\r\n')
-	content_length = L_slice[L_slice.find(': ')+2:R_slice]	
-
-	L_slice = part_file_content[index_finder(part_file_content,'Content-Wrote:'):]
-	R_slice = L_slice.find('\r\n')
-	content_wrote = L_slice[L_slice.find(': ')+2:R_slice]
-
-
-	if index_finder(part_file_content,'ETag:') != -1:
-		L_slice = part_file_content[index_finder(part_file_content,'ETag:'):]
-		R_slice = L_slice.find('\r\n')
-		etag = L_slice[L_slice.find(': ')+2:R_slice]
-	else:
-		etag = -1
-
-	if index_finder(part_file_content,'Last-Modified:') != -1:
-		L_slice = part_file_content[index_finder(part_file_content,'Last-Modified:'):]
-		R_slice = L_slice.find('\r\n')
-		last_mod = L_slice[L_slice.find(': ')+2:R_slice]
-	else:
-		last_mod = -1
-
-
-
-	""" MAKE CONNECTION """  
-	rq = make_resume_req(host_name,path,str("bytes="+content_wrote+"-"))
-	#rq = make_resume_req(host_name,path,"bytes=0-")
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	if url_object.port==None:
-		s.connect((host_name, 80))
-	else:
-		s.connect((host_name, url_object.port))
-
-	s.send(rq) 
-
-	""" RECV FIRST DATA WITHOUT HEADER """
-
-
-	header = ""
-	tail = ""
-	tail_index = 0     #use for seek to get to content
-	L_slice = ""
-	R_slice = ""
-
-	while "\r\n\r\n" not in header:
-
-		result = s.recv(1)
-		header += result
-
-
-
-	if etag or last_mod in header:
-		print "etag or last_mod = same, proceed to resume"
-
-
-	# L_slice = header[index_finder(header,'Content-Length:'):]
-	# R_slice = L_slice.find('\r\n')
-	# tail += L_slice[:R_slice]
-	# tail += '\r\n'
-	tail+= 'Content-Length: '+content_length+"\r\n"
-
-
-	L_slice = header[index_finder(header,'Accept-Ranges: bytes'):]
-	R_slice = L_slice.find('\r\n')
-	tail += L_slice[:R_slice]
-	tail += '\r\n'
-	
-
-	if index_finder(header,'ETag:') != -1:
-		L_slice = header[index_finder(header,'ETag:'):]
-		R_slice = L_slice.find('\r\n')
-		tail += L_slice[:R_slice]
-		tail += '\r\n'
-
-	if index_finder(header,'Last-Modified:') != -1:
-		L_slice = header[index_finder(header,'Last-Modified:'):]
-		R_slice = L_slice.find('\r\n')
-		tail += L_slice[:R_slice]
-		tail += '\r\n'
-	else:
-		print "Etag and content_length not found, cant resume, need re download"
-
-
-
-
-
-
-	""" RECV THE REST OF CONTENT """
-
-	num_bytes_recv = int(content_wrote)
-
-
-	with open(file_name, 'a+') as f:
-		while True: # counter<10 is a tester that terminate at 10th loop
-			result = s.recv(2048)
-			if not result: 
-				break
-			f.write(result)
-			num_bytes_recv+=len(result)
-			print "number of byte recieve : ",num_bytes_recv
-
-
-	with open(file_name+"_tail.txt", 'wb') as f:
-		to_write = "Content-Wrote: "+str(num_bytes_recv)
-		f.write(tail+to_write+"\r\n")
-
-
-	
-	if int(num_bytes_recv) == int(content_length):
-		clean_up(file_name+"_tail.txt")
-		print "download complete"
-	else:
-		print "part of file is missing due to connection drop, pls resume"
-
-
-
-	s.close()
-
-
-def new_download():	
-	""" DECLARE VARIABLE """
-	file_name = str(sys.argv[2])
-	url_object = urlparse.urlparse(sys.argv[3])
-	path = url_object.path
-	url = sys.argv[3]
-	url2 = url[7:]
-	url3 = url2.find("/")
-	host_name = url2[:url3]
-
-	""" MAKE CONNECTION """
-	rq = make_http_req(host_name,path)
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	if url_object.port==None:
-		s.connect((host_name, 80))
-	else:
-		s.connect((host_name, url_object.port))
-
-	s.send(rq) 
-
-	""" RECV FIRST DATA WITHOUT HEADER """
-	
-	header = ""
-	tail = ""
-	tail_index = 0     #use for seek to get to content
-	L_slice = ""
-	R_slice = ""
-
-	while "\r\n\r\n" not in header:
-		result = s.recv(1)
-		header += result
-
-
-	L_slice = header[index_finder(header,'Content-Length:'):]
-	R_slice = L_slice.find('\r\n')
-	content_length = L_slice[:R_slice]
-	tail += content_length
-	tail += '\r\n'
-
-
-	L_slice = header[index_finder(header,'Accept-Ranges: bytes'):]
-	R_slice = L_slice.find('\r\n')
-	tail += L_slice[:R_slice]
-	tail += '\r\n'
-	
-
-	if index_finder(header,'ETag:') != -1:
-		L_slice = header[index_finder(header,'ETag:'):]
-		R_slice = L_slice.find('\r\n')
-		tail += L_slice[:R_slice]
-		tail += '\r\n'
-
-	if index_finder(header,'Last-Modified:') != -1:
-		L_slice = header[index_finder(header,'Last-Modified:'):]
-		R_slice = L_slice.find('\r\n')
-		tail += L_slice[:R_slice]
-		tail += '\r\n'
-	else:
-		print "Etag and content_length not found, cant resume, need re download"
-
-
-
-		
-
-
-	""" RECV THE REST OF CONTENT """
-	num_bytes_recv = 0
-
-	
-	with open(file_name, 'wb') as f:
-		while True: # counter<10 is a tester that terminate at 10th loop
-			result = s.recv(2048)
-			if not result: 
-				break
-			f.write(result)
-			num_bytes_recv+=len(result)
-
-			print "number of byte recieve : ",num_bytes_recv
-
-	with open(file_name+"_tail.txt", 'wb') as f:
-		to_write = "Content-Wrote: "+str(num_bytes_recv)
-		f.write(tail+to_write+"\r\n")
-
-	content_length = content_length[16:]
-	print num_bytes_recv,content_length	
-	if int(num_bytes_recv) == int(content_length):
-		clean_up(file_name+"_tail.txt")
-		print "download complete"
-	else:
-		print "part of file is missing due to connection drop, pls resume"
-
-
-	s.close()
-
-
-def main():
-	if check_input() == True:
-		print "input accepted"
-		if check_file_exist(sys.argv[2]):
-			print "downloadfile exist, now checking if it can resume"
-			if check_file_exist(sys.argv[2]+"_tail.txt"):
-				if 'Accept-Ranges: bytes' not in get_resume_info():
-					print "this server doesnt support accept-range: bytes, you must re-download the whole thing"#0=content_length 1=accept-range 2=identifier 3=content wrote
-				else:	
-					print "hey this server support resume, lets resume"
-	
-					resume_download(get_resume_info())
-				
-			else:
-				print "part file is missing, download is done"
-			
-		else:
-			print "no such file exist"
-			print "proceed to downloading"
-			new_download()
-main()
-		
+    dirr = os.getcwd()
+    if os.path.isfile(dirr + "/" + file_name) == True:
+        return True
+    else:
+        return False
+
+
+def parser(url):
+    url_obj = urlparse.urlparse(url)
+    path = url_obj.path
+    port = url_obj.port
+    host_name = url_obj.hostname
+    return host_name, port, path
+
+
+def get_header_as_dict(url):
+    host_name, port, path = parser(url)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    if not port:
+        s.connect((host_name, 80))
+        rq = make_http_req(host_name, path)
+    else:
+        s.connect((host_name, port))
+        rq = make_http_req(host_name, path)
+    s.send(rq)
+    print rq
+    header = ""
+    while "\r\n\r\n" not in header:
+        result = s.recv(1)
+        header += result
+    data = header.replace("\n ", " ").splitlines()
+    headers = {}
+    for line in data:
+        split_here = line.find(":")
+        headers[line[:split_here]] = line[split_here:]
+
+    return headers#,content_length
+
+
+def part_download(file_name, url, b_start, b_end, meta_array, index):
+
+    host_name, port, path = parser(url)
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.settimeout(5)
+        if not port:
+            s.connect((host_name, 80))
+        else:
+            s.connect((host_name, port))
+        if b_end == "-":
+
+            rq = make_resume_req(host_name, path, "bytes=" +
+                                 b_start + "-")  # b start and end
+        else:
+            rq = make_resume_req(host_name, path, "bytes=" +
+                                 b_start + "-" + b_end)  # b start and end
+
+        s.send(rq)
+    except Exception:
+        print"connect error"
+        meta_array[index] = False
+    header = ""
+    try:
+        while "\r\n\r\n" not in header:
+            s.settimeout(5)
+            result = s.recv(1)
+            header += result
+    except Exception:
+        print"error conencting"
+        meta_array[index] = False
+
+
+    bytes_recv = 0
+    try:
+        with open(file_name, 'a+') as f:
+            while True:  # counter<10 is a tester that terminate at 10th loop
+                s.settimeout(5)
+                result = s.recv(BUFF_SIZE)
+                if not result:
+                    break
+                f.write(result)
+                bytes_recv += len(result)
+        meta_array[index] = True
+        # print "number of byte recieve : ", bytes_recv
+    except IOError:
+        print"writing progress interrupt"
+        meta_array[index] = False
+    except socket.timeout:
+        print("timeout error")
+        meta_array[index] = False
+    except socket.error:
+        print("socket error occured: ")
+        meta_array[index] = False
+    except Exception as m:
+        print m
+        meta_array[index] = False
+    s.close()
+
+
+def combine_file(files, outfile):  # files = array of file name
+    dirr = os.getcwd()
+    with open(outfile, 'a+') as f:
+        for fname in files:
+            with open(fname) as infile:
+                for line in infile:
+                    f.write(line)
+            os.remove(dirr + "/" + fname)
+    print"combine successful"
+
+
+def remove_file(files):
+    dirr = os.getcwd()
+    return os.remove(dirr + "/" + files)
+
+
+
+def resume_allocator(out_file, url, chunk_range, chunk_dividier_size, thread_status_table, connection):
+    content_length = (chunk_range[1] - chunk_range[0]) + 1
+    initial_content_length = chunk_range[1]
+    #(chunk_range[1] - chunk_range[0]) + 1
+    each = int(content_length) // int(chunk_dividier_size)
+#f_name = [file_name + str(x) for x in range(connection)]
+
+    start_range = [chunk_range[0]] + [chunk_range[0] +
+                                      (each * x) for x in range(chunk_dividier_size) if x > 0]
+    end_range = [x + each - 1 for x in start_range[:-1]] + [chunk_range[1]]
+    result_range = []
+    threads = []
+    to_fetch_next = [] #if keyboard interrupt happen, this will hav what to fetch next
+    try:
+
+        for i, j in zip(start_range, end_range):
+            result_range.append([i, j])
+        for i, j, k in zip(result_range, range(connection), thread_status_table):
+            t = threading.Thread(target=thread_allocator, args=(
+                file_name, url, i, connection, thread_status_table))
+
+            to_fetch_next = i
+            print "sending big thread : ", i
+            t.start()
+            while t.isAlive():
+                t.join(1)
+            #t.join()
+            for o in thread_status_table:
+                if o == False:
+                    print"error found, breaking loop"
+                    with open('thread_meta.txt', "wb+") as f:
+                        strr1 = str(i[0]) + "," + str(initial_content_length)
+                        f.write(strr1)
+
+                    my_dir = os.getcwd()
+                    for fname in os.listdir(my_dir):
+                        if fname.startswith("part"):
+                            os.remove(os.path.join(my_dir, fname))
+                    return
+
+    except KeyboardInterrupt as m:
+        print "keyboard interrupt caught"
+        if to_fetch_next[1] < initial_content_length: #download not done yet, it was interrupted by keyboard
+            with open('thread_meta.txt', "wb+") as f:
+                strr1 = str(to_fetch_next[1]+1)+","+str(initial_content_length)
+                f.write(strr1)
+        else:
+            print"file download is done"
+
+    except Exception as m:
+        print "bad thing happen here"
+    print "DONE DOWNLOADING"
+
+
+def isReady(header_dict): #take input as dict
+    key = header_dict.keys()
+    if 'Transfer-Encoding' in key:
+        print "chunk encoding found, this server doesnt support this type of tranfer, terminate program"
+        sys.exit(1)
+    if 'Content-Length' not in key:
+        print "No content_length found, terminate program"
+        sys.exit(1)
+    if 'Last-Modified' not in key:
+        print "Last-Modified not presented, cant check if the file is the same one, terminate program"
+        sys.exit(1)
+
+
+
+
+def thread_allocator(file_name, url, chunk_range, connection, meta_array):
+    content_length = (chunk_range[1] - chunk_range[0]) + 1
+    #content_length = chunk_range[1]
+    each = int(content_length) // int(connection)
+    f_name = ["part_" + file_name + str(x) for x in range(connection)]
+    start_range = [chunk_range[0]] + [chunk_range[0] +
+                                      (each * x) for x in range(connection) if x > 0]
+    end_range = [x + each - 1 for x in start_range[:-1]] + [chunk_range[1]]
+    threads = []
+    try:
+        for i, j, k, l in zip(f_name, start_range, end_range, range(len(meta_array))):
+            t = threading.Thread(target=part_download, args=(
+                str(i), url, str(j), str(k), meta_array, l))
+            threads.append(t)
+            print ("sending small thread : ({a}, {b})").format(a=j, b=k)
+            t.start()
+        for t in threads:
+            t.join()
+    except KeyboardInterrupt:
+        print"keyboardinterript in small thread"
+    except Exception:
+        print"something is wrong to small thread"
+    print meta_array
+
+    counter = 0
+
+    for i in range(len(meta_array)):
+        if meta_array[i]:
+            counter += 1
+
+    if counter == connection:
+        combine_file(f_name, file_name)
+        print "combine successful"
+    else:
+        print "part of file is missing"
+
+if __name__ == "__main__":
+    BUFF_SIZE = 2048
+    url = "http://speedtest.ftp.otenet.gr/files/test10Mb.db"
+    header_dict = get_header_as_dict(url)  # change it to range
+    file_name = "para.mp4"
+    isReady(header_dict)
+    content_length = header_dict['Content-Length'][2:]
+    content_range = [0, int(content_length) - 1]
+    connection = 10
+    thread_status = [None] * connection
+    if check_file_exist('thread_meta.txt'):
+        if check_file_exist('identify_meta.txt'):
+            result = []
+            with open('thread_meta.txt', "rb") as fp:
+                for i in fp.readlines():
+                    tmp = i.split(",")
+
+                    result.append(int(tmp[0]))
+                    result.append(int(tmp[1]))
+
+            chunk_range = result
+            resume_allocator(file_name, url, chunk_range, 5, thread_status, connection)
+        else:
+            print "file identifier not found please delete the downloaded file and re download"
+
+    else:
+        resume_allocator(file_name, url, content_range, 5, thread_status,
+                         connection)  # 5 is the slice piece
